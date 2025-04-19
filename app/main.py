244e218d26057c460ed49fadb3bddbe97f3cb47d
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-19 10:08:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from __future__ import annotations
+from app.cafe import Cafe
+from app.errors import NotWearingMaskError, VaccineError
+
+
+def go_to_cafe(self, friends: list[dict], cafe: Cafe):
+    masks_to_buy = 0
+    for visitor in friends:
+        try:
+            self.visit_cafe(visitor)
+        except VaccineError:
+            print("All friends should be vaccinated")
+        except NotWearingMaskError:
+            masks_to_buy += 1
+            if masks_to_buy > 0:
+                f"Friends should buy {masks_to_buy} masks"
+        else:
+            f"Friends can go to {cafe.name}"
