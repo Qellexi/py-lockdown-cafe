c892f950fad3424320c6719fdf6f51b7a3b48bd0
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-20 03:48:24 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from app.cafe import Cafe
-from app.errors import NotWearingMaskError, VaccineError
+from cafe import Cafe
+from errors import NotWearingMaskError, VaccineError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe):
@@ -13,6 +13,5 @@ def go_to_cafe(friends: list[dict], cafe: Cafe):
         except NotWearingMaskError:
             masks_to_buy += 1
             if masks_to_buy > 0:
-                f"Friends should buy {masks_to_buy} masks"
-        else:
-            return f"Friends can go to {cafe.name}"
+                print(f"Friends should buy {masks_to_buy} masks")
+    return f"Friends can go to {cafe.name}"
